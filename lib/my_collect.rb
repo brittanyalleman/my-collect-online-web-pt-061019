2ba6985cd963c7558d4ecc8b6207713181322b98
Brittany Alleman <brittany.alleman1@gmail.com>
2019-06-13 18:35:19 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,11 @@
 #require'pry'
-def my_collect(empty_array)
+def my_collect(array)
  i = 0
+ new_array = []
  #binding.pry
- while i < empty_array.length
-   yield (empty_array[i])
+ while i < array.length
+   yield (array[i])
    i+=1
+   array >>
  end
 end
