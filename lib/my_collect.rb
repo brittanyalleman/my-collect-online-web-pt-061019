644b39d925bed114760328a1b38dc21d1a14fecc
Brittany Alleman <brittany.alleman1@gmail.com>
2019-06-13 18:13:08 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,8 @@
-
-
+def my_collect(languages)
+ i = 0
+ while i < languages.upcase
+   yield (array[i])
+   i+=1
+end
+end
+end
--- spec/my_collect_spec.rb
+++ spec/my_collect_spec.rb
@@ -4,7 +4,7 @@ describe "my_collect" do
 
   it "can handle an empty collection" do
     empty_array = []
-    counter = 0
+    counter = 0 
       my_collect(empty_array) do |x|
         counter += 1
       end
